a59d3bf59e2ce9389a4679a151cb2707ec1ff0de
Garrett Brown <themagnificentmrb@gmail.com>
2015-02-10 14:26:49 -0800

    Move linux gobals into JOYSTICK namespace

--- addons/peripheral.joystick/src/api/linux/JoystickInterfaceLinux.cpp
+++ addons/peripheral.joystick/src/api/linux/JoystickInterfaceLinux.cpp
@@ -61,29 +61,32 @@
 using namespace ADDON;
 using namespace JOYSTICK;
 
-static const char *axis_names[ABS_MAX + 1] =
+namespace JOYSTICK
 {
-  "X",     "Y",     "Z",     "Rx",    "Ry",    "Rz",    "Throttle", "Rudder",
-  "Wheel", "Gas",   "Brake", "?",     "?",     "?",     "?",        "?",
-  "Hat0X", "Hat0Y", "Hat1X", "Hat1Y", "Hat2X", "Hat2Y", "Hat3X",    "Hat3Y",
-  "?",     "?",     "?",      "?",    "?",     "?",     "?",
-};
+  static const char *axis_names[ABS_MAX + 1] =
+  {
+    "X",     "Y",     "Z",     "Rx",    "Ry",    "Rz",    "Throttle", "Rudder",
+    "Wheel", "Gas",   "Brake", "?",     "?",     "?",     "?",        "?",
+    "Hat0X", "Hat0Y", "Hat1X", "Hat1Y", "Hat2X", "Hat2Y", "Hat3X",    "Hat3Y",
+    "?",     "?",     "?",      "?",    "?",     "?",     "?",
+  };
 
-static const char *button_names[KEY_MAX - BTN_MISC + 1] =
-{
-  "Btn0",       "Btn1",     "Btn2",      "Btn3",      "Btn4",      "Btn5",     "Btn6",
-  "Btn7",       "Btn8",     "Btn9",      "?",         "?",         "?",        "?",
-  "?",          "?",        "LeftBtn",   "RightBtn",  "MiddleBtn", "SideBtn",  "ExtraBtn",
-  "ForwardBtn", "BackBtn",  "TaskBtn",   "?",         "?",         "?",        "?",
-  "?",          "?",        "?",         "?",         "Trigger",   "ThumbBtn", "ThumbBtn2",
-  "TopBtn",     "TopBtn2",  "PinkieBtn", "BaseBtn",   "BaseBtn2",  "BaseBtn3", "BaseBtn4",
-  "BaseBtn5",   "BaseBtn6", "BtnDead",   "BtnA",      "BtnB",      "BtnC",     "BtnX",
-  "BtnY",       "BtnZ",     "BtnTL",     "BtnTR",     "BtnTL2",    "BtnTR2",   "BtnSelect",
-  "BtnStart",   "BtnMode",  "BtnThumbL", "BtnThumbR", "?",         "?",        "?",
-  "?",          "?",        "?",         "?",         "?",         "?",        "?",
-  "?",          "?",        "?",         "?",         "?",         "?",        "?",
-  "WheelBtn",   "Gear up",
-};
+  static const char *button_names[KEY_MAX - BTN_MISC + 1] =
+  {
+    "Btn0",       "Btn1",     "Btn2",      "Btn3",      "Btn4",      "Btn5",     "Btn6",
+    "Btn7",       "Btn8",     "Btn9",      "?",         "?",         "?",        "?",
+    "?",          "?",        "LeftBtn",   "RightBtn",  "MiddleBtn", "SideBtn",  "ExtraBtn",
+    "ForwardBtn", "BackBtn",  "TaskBtn",   "?",         "?",         "?",        "?",
+    "?",          "?",        "?",         "?",         "Trigger",   "ThumbBtn", "ThumbBtn2",
+    "TopBtn",     "TopBtn2",  "PinkieBtn", "BaseBtn",   "BaseBtn2",  "BaseBtn3", "BaseBtn4",
+    "BaseBtn5",   "BaseBtn6", "BtnDead",   "BtnA",      "BtnB",      "BtnC",     "BtnX",
+    "BtnY",       "BtnZ",     "BtnTL",     "BtnTR",     "BtnTL2",    "BtnTR2",   "BtnSelect",
+    "BtnStart",   "BtnMode",  "BtnThumbL", "BtnThumbR", "?",         "?",        "?",
+    "?",          "?",        "?",         "?",         "?",         "?",        "?",
+    "?",          "?",        "?",         "?",         "?",         "?",        "?",
+    "WheelBtn",   "Gear up",
+  };
+}
 
 CJoystickInterfaceLinux::CJoystickInterfaceLinux(void)
  : CJoystickInterface(INTERFACE_LINUX)
