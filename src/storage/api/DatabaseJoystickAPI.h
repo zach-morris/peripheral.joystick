e182a0ed87bb40a0932ed6e9f141522c963fa21a
Garrett Brown <themagnificentmrb@gmail.com>
2016-04-14 22:23:13 -0700

    Fix missing constructor body

--- src/storage/api/DatabaseJoystickAPI.h
+++ src/storage/api/DatabaseJoystickAPI.h
@@ -26,7 +26,7 @@ namespace JOYSTICK
   class CDatabaseJoystickAPI : public IDatabase
   {
   public:
-    CDatabaseJoystickAPI(void);
+    CDatabaseJoystickAPI(void) { }
 
     virtual ~CDatabaseJoystickAPI(void) { }
 
