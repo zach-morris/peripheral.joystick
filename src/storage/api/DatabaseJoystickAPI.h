cdc1deb9064d5b553f6324f92ad1966f7f431b2b
Garrett Brown <themagnificentmrb@gmail.com>
2016-04-17 02:06:58 -0700

    Redo e182a0e using c++11 idiom

--- src/storage/api/DatabaseJoystickAPI.h
+++ src/storage/api/DatabaseJoystickAPI.h
@@ -26,7 +26,7 @@ namespace JOYSTICK
   class CDatabaseJoystickAPI : public IDatabase
   {
   public:
-    CDatabaseJoystickAPI(void) { }
+    CDatabaseJoystickAPI(void) = default;
 
     virtual ~CDatabaseJoystickAPI(void) { }
 
